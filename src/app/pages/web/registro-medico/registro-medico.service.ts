7420aa0c8498eccb514323e1b1f012c9cf839cef
oscar cortes <oe.cortes@uniandes.edu.co>
2023-02-06 00:22:21 -0500

    Integracion backend sprint 1

--- src/app/pages/web/registro-medico/registro-medico.service.ts
+++ src/app/pages/web/registro-medico/registro-medico.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { RegistroMedicoBackendInterface } from 'src/app/interfaces/registro-medico.interface';
 import { environment } from 'src/environments/environment';
+import { RegistroMedicoFormInterface } from 'src/app/interfaces/registro-medico.interface';
 
 const API_AUTH_URL = environment.API.API_URL + environment.API.API_REGISTRO_MEDICO_ENDPOINT;
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,18 @@ export class RegistroMedicoService {
 
   constructor(private http: HttpClient) { }
 
-  registroMedico(registroMedico:RegistroMedicoBackendInterface,licenciaMedica:File): Observable<any> {
-    return this.http.post<any>(API_AUTH_URL, registroMedico);
+  registro(registerData:RegistroMedicoFormInterface,file:File):Observable<any> {
+    const form = new FormData();
+    console.log(registerData);
+    console.log(file)
+    form.append("medical_license", file);
+    form.append("email", registerData.email);
+    form.append("names", registerData.nombre);
+    form.append("nationality", registerData.nacionalidad);
+    form.append("speciality", registerData.especialidad);
+    form.append("password", registerData.password);
+    console.log(JSON.stringify(form));
+
+    return this.http.post<any>(API_AUTH_URL, form);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
     API_REGISTRO_USUARIO_ENDPOINT: 'patient-register',
     API_LOGIN_USUARIO_ENDPOINT: 'patient-login',
     API_REGISTRO_MEDICO_ENDPOINT: 'doctor-register',
-    API_LOGIN_MEDICO_ENDPOINT: 'patient-login',
+    API_LOGIN_MEDICO_ENDPOINT: 'doctor-login',
     
   }
 };
