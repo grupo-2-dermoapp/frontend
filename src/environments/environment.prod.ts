bdf6db6093c5fd04aaeddd2257e0ac7b99618b97
oscar cortes <oe.cortes@uniandes.edu.co>
2023-02-06 01:11:45 -0500

    Fix environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,10 @@ export const environment = {
   production: true,
   API: {
     API_URL: 'http://localhost',
-    API_AUTH_ENDPOINT: '/auth',
+    API_PERFIL_DERMATOLOGICO_ENDPOINT: 'dermo-profile-register',
+    API_REGISTRO_USUARIO_ENDPOINT: 'patient-register',
+    API_LOGIN_USUARIO_ENDPOINT: 'patient-login',
+    API_REGISTRO_MEDICO_ENDPOINT: 'doctor-register',
+    API_LOGIN_MEDICO_ENDPOINT: 'doctor-login'
   }
 };
