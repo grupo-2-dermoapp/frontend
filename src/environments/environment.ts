761fd795c4d7036e8ce1d9e852d61a1fc4ded5b3
oscar cortes <oe.cortes@uniandes.edu.co>
2023-02-26 11:28:13 -0500

    Android integration and some fix images

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   API: {
-    AUTH_API_URL: 'http://localhost:8080/dermoapp/auth/v1/',
-    MEDICAL_CASES_URL: 'http://localhost:8080/dermoapp/medical-cases/v1/',
+    AUTH_API_URL: 'https://bhuux5lyc1.execute-api.us-east-2.amazonaws.com/dermoapp/auth/v1/',
+    MEDICAL_CASES_URL: 'https://bhuux5lyc1.execute-api.us-east-2.amazonaws.com/dermoapp/medical-cases/v1/',
     API_PERFIL_DERMATOLOGICO_ENDPOINT: 'dermo-profile-register',
     API_REGISTRO_USUARIO_ENDPOINT: 'patient-register',
     API_LOGIN_USUARIO_ENDPOINT: 'patient-login',
