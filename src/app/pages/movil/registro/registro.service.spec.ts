c9d45caf93c2fb9f79b1d6d474f0bde408750501
oscar cortes <oe.cortes@uniandes.edu.co>
2023-03-12 22:42:12 -0500

    correción de test unitario

--- src/app/pages/movil/registro/registro.service.spec.ts
+++ src/app/pages/movil/registro/registro.service.spec.ts
@@ -1,4 +1,7 @@
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RegistroUsuarioBackendInterface } from 'src/app/interfaces/registro-usuario.interface';
 import { environment } from 'src/environments/environment';
@@ -8,11 +11,13 @@ import { RegistroService } from './registro.service';
 describe('RegistroService', () => {
   let service: RegistroService;
   let httpMock: HttpTestingController;
-  const API_REGISTRO_USUARIO_ENDPOINT = environment.API.AUTH_API_URL + environment.API.API_REGISTRO_USUARIO_ENDPOINT;
+  const API_REGISTRO_USUARIO_ENDPOINT =
+    environment.API.AUTH_API_URL +
+    environment.API.API_REGISTRO_USUARIO_ENDPOINT;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(RegistroService);
     httpMock = TestBed.inject(HttpTestingController);
@@ -30,12 +35,13 @@ describe('RegistroService', () => {
       age: 0,
       location: '',
       dermatological_profile_uuid: '',
-    }
+      notification_token: '',
+    };
 
     service.registro(user).subscribe();
 
     const req = httpMock.expectOne(`${API_REGISTRO_USUARIO_ENDPOINT}`);
-    expect(req.request.method).toBe("POST");
+    expect(req.request.method).toBe('POST');
     req.flush('');
   });
 });
